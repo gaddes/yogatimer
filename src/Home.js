7beb9473903386ff4e73ca1ab7974d228cf5d409
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-02-24 10:28:56 +0000

    Complete s1 (feature) - Create cancel button

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,39 @@ import React, { Component } from 'react'
 import './home.scss'
 import chime from './audio/chime.mp3';
 
-const setCountdown = (totalTime) => {
-  // Keep track of initial time
-  const initialTime = totalTime;
+// Variable used to start and stop the countdown timer
+let intervalID;
 
-  // Make buttons inactive
-  let buttons = document.querySelectorAll('.timerbutton');
+const startCountdown = (timePerLoop) => {
+  // TODO: replace this with user-selected number of loops
+  let numberOfLoops = 1;
+  let totalTime = timePerLoop * numberOfLoops;
+
+  // Keep track of initial time
+  const initialTime = timePerLoop;
+
+  // TODO: extract into separate function
+  // Make timer buttons inactive
+  const buttons = document.querySelectorAll('.timerbutton');
   buttons.forEach(function(btn) {
     btn.setAttribute('disabled', true);
   });
 
+  // TODO: make all querySelector elements global
   // Grab countdown element
   let currentTime = document.querySelector('.countdown');
 
   // FIXME: for dev only
-  // totalTime = 3;
+  // timePerLoop = 3;
 
-  const countdown = () => {
-    console.log(totalTime);
+  const updateCountdown = () => {
+    console.log(`time left in this loop = ${timePerLoop}`);
+    console.log(`totalTime remaining = ${totalTime}`);
 
     let minutes;
     let seconds;
 
-    switch(totalTime) {
+    switch(timePerLoop) {
       case 0:
         // TODO: Activate buttons  
         // Play sound and reset timer
@@ -39,15 +49,15 @@ const setCountdown = (totalTime) => {
         // Display time
         currentTime.innerHTML = `${minutes}:${seconds}`;
 
-        totalTime = initialTime - 1;
+        timePerLoop = initialTime - 1;
 
         // TODO: Stop timer
         // clearInterval(timer);
         break;
 
       case 1:
-        // Reset totalTime to the length of time selected by the user
-        totalTime = totalTime - 1;
+        // Reset timePerLoop to the length of time selected by the user
+        timePerLoop = timePerLoop - 1;
         // Calculate mins/secs
         minutes = Math.floor(initialTime / 60);
         seconds = initialTime % 60;
@@ -57,22 +67,57 @@ const setCountdown = (totalTime) => {
 
       default:
         // Calculate mins/secs
-        minutes = Math.floor(totalTime / 60);
-        seconds = totalTime % 60;
+        minutes = Math.floor(timePerLoop / 60);
+        seconds = timePerLoop % 60;
 
         // Display time
         currentTime.innerHTML = `${minutes}:${seconds}`;
 
         // Decrease seconds by one
-        totalTime = totalTime - 1;
+        timePerLoop = timePerLoop - 1;
         break;
     }
   }
 
   // Call countdown function immediately on button click
-  countdown();
+  updateCountdown();
+  
   // Decrease time remaining every second
-  const timer = setInterval(countdown, 1000);
+  intervalID = setInterval(function() {
+    if (totalTime === 0) {
+      // Stop countdown
+      clearInterval(intervalID);
+
+      // TODO: extract into separate function
+      // Activate timer buttons
+      const buttons = document.querySelectorAll('.timerbutton');
+      buttons.forEach(function(btn) {
+        btn.removeAttribute('disabled');
+      });
+    } else {
+      totalTime -= 1;
+      updateCountdown();
+    }
+  }, 1000);
+}
+
+const resetCountdown = () => {
+  console.log('it works!');
+
+  // Stop countdown
+  clearInterval(intervalID);
+
+  // TODO: make all querySelector elements global
+  // Reset visual timer to 00:00
+  let currentTime = document.querySelector('.countdown');
+  currentTime.innerHTML = `0:00`;
+
+  // TODO: extract into separate function
+  // Activate timer buttons
+  const buttons = document.querySelectorAll('.timerbutton');
+  buttons.forEach(function(btn) {
+    btn.removeAttribute('disabled');
+  });
 }
 
 export default class Home extends Component {
@@ -80,14 +125,19 @@ export default class Home extends Component {
     return (
       <div>
         <audio ref={ref => this.player = ref} />
-        <h1 className='countdown'>00:00</h1>
+        <h1 className='countdown'>0:00</h1>
         <p>5 loops remaining</p>
         <p>Choose number of minutes</p>
-        <button className='timerbutton' onClick={() => setCountdown(60)}>1</button>
-        <button className='timerbutton' onClick={() => setCountdown(120)}>2</button>
-        <button className='timerbutton' onClick={() => setCountdown(180)}>3</button>
-        <button className='timerbutton' onClick={() => setCountdown(240)}>4</button>
-        <button className='timerbutton' onClick={() => setCountdown(300)}>5</button>
+        <div className="minute-buttons">
+          <button className='timerbutton' onClick={() => startCountdown(60)}>1</button>
+          <button className='timerbutton' onClick={() => startCountdown(120)}>2</button>
+          <button className='timerbutton' onClick={() => startCountdown(180)}>3</button>
+          <button className='timerbutton' onClick={() => startCountdown(240)}>4</button>
+          <button className='timerbutton' onClick={() => startCountdown(300)}>5</button>
+        </div>
+        <div className="cancel-button">
+          <button className='cancelbutton' onClick={() => resetCountdown()}>Cancel</button>
+        </div>
       </div>
     )
   }
